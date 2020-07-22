9bca6c570d198615c479b8df623d2c7785952c5d
luqiqi <2246483156@qq.com>
2020-07-22 10:56:25 +0800

    规定了第一版的接口

---
+++ src/main/java/org/king2/luqiqi/cache/interfaces/Cache.java
@@ -0,0 +1,37 @@
+package org.king2.luqiqi.cache.interfaces;
+
+import com.alibaba.fastjson.JSON;
+
+import java.io.File;
+
+/**
+ * 项目名称：CACHE-KING2-V1.0
+ * 类 名 称：Cache
+ * 类 描 述：WebKingCache的统一接口
+ * 创建时间：2020/7/22 10:43 上午
+ * 创 建 人：俞烨-company-mac
+ */
+public interface Cache {
+
+
+    void set(String key, Object value);
+
+
+    <T> T get(String key, Class<T> clazz);
+
+    default void remove(String key) {
+
+    }
+
+    default boolean exits(String key) {
+        return false;
+    }
+
+    default boolean expired(String key) {
+        return false;
+    }
+
+    default void setEx(String key, long timeout, Object value) {
+
+    }
+}
