9e1e52a7a16a130ddcfcfef69a31dbee9d46b91c
luqiqi <2246483156@qq.com>
2020-07-22 10:41:23 +0800

    清空

--- src/main/java/org/king2/webkcache/cache/exceptions/BeanFactoryCaseError.java
+++
@@ -1,8 +0,0 @@
-package org.king2.webkcache.cache.exceptions;
-
-public class BeanFactoryCaseError extends Exception {
-
-    public BeanFactoryCaseError(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/org/king2/webkcache/cache/interfaces/HashWebKingCache.java
+++
@@ -1,6 +0,0 @@
-package org.king2.webkcache.cache.interfaces;
-
-public interface HashWebKingCache extends WebKingCache {
-
-    void set(String allKey, String oneKey, String value);
-}
